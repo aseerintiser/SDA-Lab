81e986783788476606127f34f73201839af141c9
Aseer Intiser <56443166+aseerintiser@users.noreply.github.com>
2021-12-01 12:14:56 +0600

    new commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+
+
+app.get('/',(req,res)=> {
+    res.send('Welcome azure')
+})
+
+
+
+
+app.listen(8080, ()=> {
+    console.log('Server is listening on port 8080....')
+})
